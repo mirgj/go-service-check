6fe4845c5a40d4e9bdc31d90e7e057fa80965be9
mirgj <mirash.gjolaj@gmail.com>
2019-09-16 22:58:02 +0800

    refactor factory

--- notifierFactory.go
+++ notifierFactory.go
@@ -5,14 +5,16 @@ type Notifier interface {
 	Notify(url string)
 }
 
-func getNotifier(nType string) Notifier {
-	var notifier Notifier = Console{}
+var notifiers = map[string]Notifier{
+	"smtp":     &SMTP{},
+	"sendgrid": &SendGrid{},
+	"console":  &Console{},
+}
 
-	if nType == "smtp" {
-		notifier = SMTP{}
-	} else if nType == "sendgrid" {
-		notifier = SendGrid{}
+func getNotifier(nType string) Notifier {
+	if notifer, ok := notifiers[nType]; ok {
+		return notifer
 	}
 
-	return notifier
+	return notifiers["console"]
 }
